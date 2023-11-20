4232d51c7ada98f393334b6ea7c5fd28a265ae47
James Ward <jward11235@gmail.com>
2023-11-20 15:00:41 +0000

    Added neighbour checker

--- src/utils/util_functions.jl
+++ src/utils/util_functions.jl
@@ -1,6 +1,7 @@
 module Utils
 
 import ..Types: WorldState, AgentState, Node, Position
+using Graphs
 
 function pos_distance(p1::Position, p2::Position)
     return ((p1.x - p2.x)^2 + (p1.y - p2.y)^2)^0.5
@@ -18,5 +19,29 @@ function operate_pos(p::Position, n::Number, f)
     return Position(f(p.x, n), f(p.y, n))
 end
 
+function get_neighbours(agent_pos::Union{AbstractEdge, Int64}, world::WorldState, no_dummy_nodes::Bool)
+    if agent_pos isa Int64
+        neighbours = neighbors(world.map, agent_pos)
+    elseif has_edge(world.map, dst(agent_pos), src(agent_pos))
+        neighbours = [src(agent_pos), dst(agent_pos)]
+    else
+        return [dst(agent_pos)]
+    end
+    # Dummy node check - ensures that only real nodes will be returned
+    if no_dummy_nodes
+        for i in 1:length(neighbours)
+            if neighbours[i] > world.n_nodes
+                next_neighbours  = neighbors(world.map, neighbours[i])
+                for j in 1:length(next_neighbours)
+                    if !(next_neighbours[j] in neighbours)
+                        neighbours[i] = next_neighbours[j]
+                    end
+                end
+            end
+        end
+    end
+    
+    return neighbours
+end
 
 end
