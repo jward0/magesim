b70b5cfb9e4b368ec36a13da37dfc81ad857d51d
James Ward <jward11235@gmail.com>
2024-01-04 16:47:57 +0000

    Updated get_neighbours function

--- src/utils/util_functions.jl
+++ src/utils/util_functions.jl
@@ -21,26 +21,34 @@ end
 
 function get_neighbours(agent_pos::Union{AbstractEdge, Int64}, world::WorldState, no_dummy_nodes::Bool)
     if agent_pos isa Int64
-        neighbours = neighbors(world.map, agent_pos)
+        neighbours = copy(neighbors(world.map, agent_pos))
     elseif has_edge(world.map, dst(agent_pos), src(agent_pos))
         neighbours = [src(agent_pos), dst(agent_pos)]
     else
         return [dst(agent_pos)]
     end
-    # Dummy node check - ensures that only real nodes will be returned
+
     if no_dummy_nodes
-        for i in 1:length(neighbours)
-            if neighbours[i] > world.n_nodes
-                next_neighbours  = neighbors(world.map, neighbours[i])
-                for j in 1:length(next_neighbours)
-                    if !(next_neighbours[j] in neighbours)
-                        neighbours[i] = next_neighbours[j]
-                    end
-                end
+        # Will currently break if agent_pos is an edge
+        log::Array{Int64, 1} = []
+
+        function iterate(node, previous_node)
+            if node <= world.n_nodes
+                push!(log, node)
+            else
+                next_neighbours = copy(neighbors(world.map, node))
+                filter!(e -> e != previous_node, next_neighbours)
+                iterate(next_neighbours[1], node)
             end
         end
+
+        for n in neighbours
+            iterate(n, agent_pos)
+        end
+
+        neighbours = unique(log)
     end
-    
+
     return neighbours
 end
 
